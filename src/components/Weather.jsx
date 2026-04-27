fbb53b5b5cc2181f903c5a5f86ef9a96b33c693d
Abdul Qayyum Waseem Khan <qayyuma089@gmail.com>
2026-04-27 13:50:41 +0500

    check one two three

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 export default function Weather() {
   const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(false);
   const apiKey = import.meta.env.VITE_APP_APIKEY;
   const handleFetchWeather = async (e) => {
     e.preventDefault();
@@ -11,7 +12,10 @@ export default function Weather() {
       );
       const data = await response.json();
       console.log(data);
-    } catch (error) {}
+      setWeatherData(data);
+    } catch (error) {
+      console.log("Getting error while fetching weather data", error.message);
+    }
   };
   return (
     <div>
@@ -24,6 +28,14 @@ export default function Weather() {
         />
         <button type="submit">Get Weather</button>
       </form>
+      <div>
+        {weatherData && (
+          <div>
+            <h1>{(weatherData.main.temp - 273.12).toFixed(2)}&deg;C</h1>
+            <h2>⛅{weatherData.weather[0].description}</h2>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
